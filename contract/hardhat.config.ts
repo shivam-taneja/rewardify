387d19774eb488a7098828631ba53d0c6430655f
Shivam Taneja <shiivamtaneja@email.com>
2025-11-01 12:40:03 +0530

    chore: setup monad network
    Signed-off-by: Shivam Taneja <shiivamtaneja@email.com>

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,17 @@
-import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import type { HardhatUserConfig } from "hardhat/config";
+
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    monad: {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
 };
 
 export default config;
